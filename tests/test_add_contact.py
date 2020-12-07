996ab1b35c0be7132bf0130dc2fa9331aa4b992f
Redbu11 <Redbu11dev777@gmail.com>
2020-12-07 17:16:54 +0700

    removed unused imports

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,11 +1,8 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from models.contact import Contact
 import unittest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestAddContact(unittest.TestCase):
