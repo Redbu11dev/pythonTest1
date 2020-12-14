890cbf1db02e8e375c800cc368cd5037b38393dc
Redbu11 <bukovets@svyazcom.ru>
2020-12-14 15:47:52 +0700

    added randomized contact creation, home task 15 done

--- addressbook_tester/src/tests/test_add_contact.py
+++ addressbook_tester/src/tests/test_add_contact.py
@@ -1,4 +1,7 @@
 import os
+import pytest
+import random
+import string
 from addressbook_tester.src.models.contact import Contact
 
 def random_string(prefix, maxlen):
@@ -8,36 +11,44 @@ def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    image_path = os.path.join(os.path.dirname(__file__), '../resources/images/grapefruit-slice-332-332.jpg').replace("/", "\\")
-    contact1 = Contact("First name",
-                       "Middle name",
-                       "Ivanov",
-                       "Nickname",
-                       image_path,
-                       "Title",
-                       "Company",
-                       "Address",
-                       "222-22-22",
-                       "8(800)2000600",
-                       "2000000",
-                       "2111111",
-                       "email1@mail.com",
-                       "email2@mail.com",
-                       "email3@mail.com",
-                       "www.google.com",
-                       "1",
-                       "January",
-                       "1990",
-                       "1",
-                       "February",
-                       "1867",
-                       None,  # "group1"
-                       "addr2",
-                       "+9999999",
-                       "some notes"
+testdata = [
+        # Group(name=random_string("name", 10).strip().replace("/  +/g", " "),
+        #       header=random_string("header", 20).strip().replace("/  +/g", " "),
+        #       footer=random_string("footer", 20).strip().replace("/  +/g", " "))
+        Contact(first_name=random_string("first_name", 10).strip().replace("/  +/g", " "),
+                       middle_name="Middle name",
+                       last_name=random_string("last_name", 10).strip().replace("/  +/g", " "),
+                       nickname="Nickname",
+                       photo_path=None,
+                       title="Title",
+                       company="Company",
+                       address=random_string("address", 10).strip().replace("/  +/g", " "),
+                       home_phone_number="222-22-22",
+                       mobile_phone_number="8(800)2000600",
+                       work_phone_number="2000000",
+                       fax_number="2111111",
+                       email_1="email1@mail.com",
+                       email_2="email2@mail.com",
+                       email_3="email3@mail.com",
+                       homepage="www.google.com",
+                       birthday_day="1",
+                       birthday_month="January",
+                       birthday_year="1990",
+                       anniversary_day="1",
+                       anniversary_month="February",
+                       anniversary_year="1867",
+                       group_name=None,  # "group1"
+                       address_2="addr2",
+                       phone_2="+9999999",
+                       notes="some notes"
                        )
+        for i in range(5)
+    ]
+
+@pytest.mark.parametrize("contact1", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact1):
+    old_contacts = app.contact.get_contact_list()
+    # image_path = os.path.join(os.path.dirname(__file__), '../resources/images/grapefruit-slice-332-332.jpg').replace("/", "\\")
     app.contact.create(contact1)
     app.contact.return_to_contacts_page()
     # app.contact.verify_account_created(contact1)
