61314f3e672ff5b1a6edb2c5debec779b95cf93b
Redbu11 <bukovets@svyazcom.ru>
2020-12-14 16:18:08 +0700

    added code for webinar 5/08, home task 16 done

--- addressbook_tester/src/application.py
+++ addressbook_tester/src/application.py
@@ -6,12 +6,21 @@ from addressbook_tester.src.helpers.session_helper import SessionHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError(f"Unrecognized browser: {browser}")
+
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def destroy(self):
         self.wd.quit()
@@ -26,4 +35,4 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("/edit.php"):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
