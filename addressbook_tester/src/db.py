46af81bfb0264e6b0d14beffdaaa8e0975784ba8
Redbu11 <bukovets@svyazcom.ru>
2020-12-15 17:24:20 +0700

    added code for webinar 7/6

--- addressbook_tester/src/db.py
+++ addressbook_tester/src/db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+from addressbook_tester.src.models.contact import Contact
 from addressbook_tester.src.models.group import Group
 
 
@@ -20,6 +21,18 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connecton.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), first_name=firstname, last_name=lastname))
         finally:
             cursor.close()
         return list
