e0016ea4292abcd5fddcd645b44f6cf2e802e5e3
Redbu11 <bukovets@svyazcom.ru>
2020-12-15 12:39:49 +0700

    added code for webinar 7/3

--- addressbook_tester/src/db.py
+++ addressbook_tester/src/db.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+from addressbook_tester.src.models.group import Group
+
 
 class DbFixture:
     def __init__(self, host, name, user, password):
@@ -8,6 +10,18 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connecton = mysql.connector.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connecton.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
 
     def destroy(self):
         self.connecton.close()
