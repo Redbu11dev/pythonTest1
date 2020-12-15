98c91af424091fb5ddf00b0e0a89e460e48c8857
Redbu11 <bukovets@svyazcom.ru>
2020-12-15 16:45:34 +0700

    added code for webinar 7/4

--- addressbook_tester/src/tests/test_add_group.py
+++ addressbook_tester/src/tests/test_add_group.py
@@ -31,11 +31,13 @@ from addressbook_tester.src.models.group import Group
 # from addressbook_tester.src.data.groups import constant as testdata
 
 # @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    # old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
     assert app.group.count() == len(old_groups) + 1
-    new_groups = app.group.get_group_list()
+    # new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
