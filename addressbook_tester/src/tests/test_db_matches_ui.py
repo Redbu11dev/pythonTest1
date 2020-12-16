7114d61a91e94ff467901d26976050ba68ad5cec
Redbu11 <bukovets@svyazcom.ru>
2020-12-16 13:14:36 +0700

    home task 21 done

--- addressbook_tester/src/tests/test_db_matches_ui.py
+++ addressbook_tester/src/tests/test_db_matches_ui.py
@@ -1,11 +1,23 @@
+from addressbook_tester.src.models.contact import Contact
 from addressbook_tester.src.models.group import Group
 from timeit import timeit
 
 
-def test_group_list(app, db):
-    print(timeit(lambda : app.group.get_group_list(), number=1))
-    def clean(group):
-        return Group(id=group.id, name=group.name.strip())
-    print(timeit(lambda :  map(clean, db.get_group_list()), number=1000))
-    # assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
-    assert False
+# def test_group_list(app, db):
+#     print(timeit(lambda : app.group.get_group_list(), number=1))
+#     def clean(group):
+#         return Group(id=group.id, name=group.name.strip())
+#     print(timeit(lambda :  map(clean, db.get_group_list()), number=1000))
+#     # assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+#     assert False
+
+def test_group_list_ui_matches_db(app, db):
+    ui_list = app.group.get_group_list()
+    db_list = db.get_group_list()
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list_ui_matches_db(app, db):
+    ui_list = app.contact.get_contact_list()
+    db_list = db.get_contact_list()
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
